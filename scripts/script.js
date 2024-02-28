bd18c4d636c3011488788710dd806e30b6cfddc9
finntendoverse <finnlambouris616@gmail.com>
2024-02-28 18:19:41 -0500

    question 1 shows when the start quiz button is clicked

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let quizStart = document.querySelector("#start-quiz");
+
+quizStart.addEventListener("click", function() {
+    let question1 = document.querySelector("#question-1");
+    question1.setAttribute("style", "display: block;");
+});
