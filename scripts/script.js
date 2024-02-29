fc6bb27e8c00e16690acb37355cc1e5e4834e2f3
finntendoverse <finnlambouris616@gmail.com>
2024-02-29 18:33:39 -0500

    automatically goes to scoreboard when time runs out

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-let quizStart = document.querySelector("#start-quiz");
+// introduction text variable
 let quizIntro = document.querySelector("#intro");
+
+// button variables
+let quizStart = document.querySelector("#start-quiz");
 let nextQuestion = document.querySelectorAll(".quiz button");
 
+// question variables
 let question1 = document.querySelector("#question-1");
 let question2 = document.querySelector("#question-2");
 let question3 = document.querySelector("#question-3");
@@ -12,19 +16,45 @@ let question7 = document.querySelector("#question-7");
 let question8 = document.querySelector("#question-8");
 let question9 = document.querySelector("#question-9");
 let question10 = document.querySelector("#question-10");
+let questions = document.querySelectorAll(".question");
+
 let scoreboard = document.querySelector("#score");
 
+// answer variables
 let wrongAnswer = document.querySelectorAll(".incorrect")
 let rightAnswer = document.querySelectorAll(".correct")
 let alert = document.querySelector("#alert");
 
-// function to start the quiz
-quizStart.addEventListener("click", function() {
-    quizIntro.setAttribute("style", "display: none;");
-    question1.setAttribute("style", "display: block;");
+// scoreboard variables
+let score = 0;
+let score1 = document.querySelector("#score-1");
+let score2 = document.querySelector("#score-2");
+let score3 = document.querySelector("#score-3");
+
+// timer variables
+let timerElement = document.querySelector("#timer");
+let time = 59;
+
+// starts the quiz when the start button is clicked
+quizStart.addEventListener("click", function() {                // WHEN the start button is clicked
+    quizIntro.setAttribute("style", "display: none;");          // THEN the introduction is removed from the page
+    question1.setAttribute("style", "display: block;");         // THEN the first question is displayed
+
+    timerElement.setAttribute("style", "display: block");       // THEN the timer is displayed
+    timerElement.textContent = 60;                              // THEN the timer text reads 60
+    let timer = setInterval(function() {
+        timerElement.textContent = time;                        // THEN the timer text reads 59
+        time = -- time;                                         // THEN the timer counts down by 1
+        
+        if (time <= -1) {
+            showScoreboard();
+        }
+        
+        return time;
+    }, 1000);                                                   // THEN this loops every second, causing the time to continually count down by 1
 });
 
-// function to cycle through the questions
+// cycles through the 10 questions and then shows the timer
 nextQuestion.forEach(button => {
     button.addEventListener("click", function() {
             let currentQuestion = button.parentElement;
@@ -65,7 +95,7 @@ nextQuestion.forEach(button => {
     });
 });
 
-// function to alert users if they answered correctly or incorrectly
+// alerts users if they answered correctly or incorrectly
 nextQuestion.forEach(button => {
     button.addEventListener("click", function() {
         let message = document.createElement("p");
@@ -81,5 +111,34 @@ nextQuestion.forEach(button => {
         setTimeout(function() {
             message.innerHTML = "";
         }, 1500);
+
+        // updates the score for correct answers, and subtracts time for incorrect answers
+        if (button.classList.contains("correct")) {
+            score = score + 10;
+            score1.textContent = "name: " + score;
+        } else {
+            time = time - 10;
+            
+            if (time <= -1) {
+                showScoreboard();
+            }
+        }
     });
 });
+
+// goes directly to scoreboard
+let showScoreboard = function() {
+    timerElement.setAttribute("style", "display: none;");
+    timerElement.setAttribute("style", "display: none;");
+    question1.setAttribute("style", "display: none;");
+    question2.setAttribute("style", "display: none;");
+    question3.setAttribute("style", "display: none;");
+    question4.setAttribute("style", "display: none;");
+    question5.setAttribute("style", "display: none;");
+    question6.setAttribute("style", "display: none;");
+    question7.setAttribute("style", "display: none;");
+    question8.setAttribute("style", "display: none;");
+    question9.setAttribute("style", "display: none;");
+    question10.setAttribute("style", "display: none;");
+    scoreboard.setAttribute("style", "display: block;");
+}
